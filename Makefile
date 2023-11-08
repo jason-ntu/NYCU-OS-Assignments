ca117c2f1861b4d3900d45f9b55402aa87fa767d
june <df41022@gmail.com>
2023-11-08 19:24:01 +0800

    add thread_info_t type

--- Makefile
+++ Makefile
@@ -4,13 +4,13 @@ compile:
 	@gcc ./sched_demo_311551137.c -o sched_demo_311551137
 
 1: compile
-	@echo "Case ${@}:"
+	@echo "\nCase ${@}:"
 	./sched_demo_311551137 -n 1 -t 0.5 -s NORMAL -p -1
 
 2: compile
-	@echo "Case ${@}:"
+	@echo "\nCase ${@}:"
 	./sched_demo_311551137 -n 2 -t 0.5 -s FIFO,FIFO -p 10,20
 
 3: compile
-	@echo "Case ${@}:"
+	@echo "\nCase ${@}:"
 	./sched_demo_311551137 -n 3 -t 1.0 -s NORMAL,FIFO,FIFO -p -1,10,30
--- sched_demo_311551137.c
+++ sched_demo_311551137.c
@@ -6,23 +6,26 @@
 
 pthread_barrier_t barrier;
 
+typedef struct {
+    pthread_t thread_id;
+    int thread_num;
+    int sched_policy;
+    int sched_priority;
+} thread_info_t;
+
 void *thread_func(void *arg)
 {
     /* 1. Wait until all threads are ready */
-    printf("Thread is ready!\n");
-
-    int result = pthread_barrier_wait(&barrier);
-
-    if (result == PTHREAD_BARRIER_SERIAL_THREAD) {
-        printf("All threads have reached the barrier.\n");
-    }
+    thread_info_t* thread_info = (thread_info_t*)arg;
+    pthread_barrier_wait(&barrier);
 
     /* 2. Do the task */ 
     for (int i = 0; i < 3; i++) {
-        // printf("Thread %d is running\n", arg[1]);
+        printf("Thread %d is running\n", thread_info->thread_num);
         /* Busy for <time_wait> seconds */
     }
     /* 3. Exit the function  */
+    return 0;
 }
 
 int main(int argc, char *argv[]) {
@@ -55,19 +58,13 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    // printf("num_threads = %d\n", num_threads);
-    // printf("time_wait = %.1f\n", time_wait);
-    // printf("policies = %s\n", policies);
-    // printf("priorities = %d\n", priorities);
-
     assert(num_threads);
     assert(time_wait);
     assert(policies);
     assert(priorities);
 
     /* 2. Create <num_threads> worker threads */
-    pthread_t thread_ids[num_threads];
-    int thread_args[num_threads];
+    thread_info_t thread_infos[num_threads];
     pthread_barrier_init(&barrier, NULL, num_threads);
     
     /* 3. Set CPU affinity */
@@ -78,13 +75,13 @@ int main(int argc, char *argv[]) {
 
     /* 5. Start all threads at once */
     for (int i = 0; i < num_threads; i++) {
-        thread_args[i] = i;
-        pthread_create(&thread_ids[i], NULL, thread_func, &thread_args[i]);
+        thread_infos[i].thread_num = i;
+        pthread_create(&(thread_infos[i].thread_id), NULL, thread_func, &thread_infos[i]);
     }
 
     /* 6. Wait for all threads to finish  */
     for (int i = 0; i < num_threads; i++) {
-        pthread_join(thread_ids[i], NULL);
+        pthread_join(thread_infos[i].thread_id, NULL);
     }
 
     pthread_barrier_destroy(&barrier);
